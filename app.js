0ea85c70808ba4530a09fb1f279a9841cb2a835b
RedHunter <arunasanjeewa1994@gmail.com>
2017-08-03 18:47:56 +0530

    second

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ const express =require('express');
 const app = express();
 const user = require('./routes/users');
 const path = require('path');
+const mongoose = require('mongoose');
 
 const port = process.env.PORT || 3000;
 
+const config = require('./config/database');
 
+const connection = mongoose.connect(config.database);
+if(connection){
+	console.log('Database Connected: OK');
+}else{
+	console.log('Database Not Connected: ERROR');
+}
 app.use(express.static(path.join(__dirname,"public")));
 
 
